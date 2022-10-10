17cc6a2f1bf87c4603e3778b11bf258ba7597b2c
acevedoluise <lea0133@hotmail.com>
2022-10-10 11:55:43 -0500

    Created folder text and inside that folder the file userData.json in order to manage the text input for the page. Modified function Testimonio() to receive object. Modified function App() to send objects in mark <Testimonio />.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './stylesheets/Testimonio.css'
 import {Testimonio} from './components/Testimonio.js'
 
 function App() {
+  let userData = require("./text/userData.json");
   return (
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros estudiantes sobre freeCodeCamp:</h1>
-        <Testimonio />
+          <Testimonio 
+          {...userData.user1} />
+          <Testimonio 
+          {...userData.user2} />
+          <Testimonio 
+          {...userData.user3} />
       </div>
     </div>
   );
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,17 +1,22 @@
 import React from "react";
-import Foto from "../pictures/testimonio-emma.png"
 
-export function Testimonio() {
+export function Testimonio(obj) {
     return (
         <div className='contenedor-testimonio'>
             <img
                 className='imagen-testimonio'
-                src={Foto}
-                alt='fotografia de Emma'/>
+                src={require(`../pictures/testimonio-${obj.imagen}.png`)}
+                alt={`fotografia de ${obj.nombre}` }/>
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-                <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-                <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+                <p className='nombre-testimonio'>
+                    <strong>{obj.nombre}</strong> en {obj.pais}
+                </p>
+                <p className='cargo-testimonio'>
+                    {obj.cargo} en <strong>{obj.empresa}</strong>
+                </p>
+                <p className='texto-testimonio'>
+                    "{obj.testimonio}"
+                </p>
             </div>
         </div>
     )
